5bc63f935b4da3f7da32e4df77168b2afcb7c0e1
Kristjan Variksoo <kristjanvariksoo@gmail.com>
2018-04-28 15:37:14 +0300

    Data from serial -> node -> page.

--- Final/index.js
+++ Final/index.js
@@ -1,6 +1,7 @@
 console.log("Importing libraries.");
 var http = require('http');
 var fs = require('fs');
+var SerialPort = require("serialport").SerialPort;
 
 // Loading the index file . html displayed to the client
 
@@ -20,17 +21,50 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection', function (socket) {
     console.log('[SOCK] Client connected!');
     console.log(socket);
+    socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+    });
 });
 
 PORT = 8080;
 server.listen(PORT);
 console.log("[HTTP] Server started on port " + PORT);
 
-var SerialPort = require("serialport").SerialPort;
-var serialport = new SerialPort("/dev/ttyUSB0");
+function ProcessLine(arr) {
+  line = arr.join("").slice(0,-2);
+  io.emit('gotline', line);
+}
+
+var SerialPort = require('serialport');
+var port = new SerialPort('/dev/ttyUSB0', {
+  baudRate: 9600
+});
+port.on("open", function(){
+  Line = [];
+  console.log("[SERIAL] Opened");
+  port.on("data", function(data){
+    chars = JSON.parse(JSON.stringify(data))["data"].splice(",");
+    chars.forEach(function(charcode) {
+      //Convert to string knowng it is ascii.
+
+      char = String.fromCharCode(charcode);
+      //console.log("[SERIAL] Got data: " + char);
+
+      Line.push(char);
+      if (char == "\n") {
+        ProcessLine(Line);
+        Line = [];
+      }
+    });
+  });
+});
+
+/*
+var serialport = SerialPort("/dev/ttyUSB0");
 serialport.on('open', function(){
   console.log('Serial Port Opend');
   serialport.on('data', function(data){
       console.log(data);
   });
 });
+*/
